4e5822937f54b5ae3bde090ced262fe7b6142619
kaushik-72 <kaushikkarmakarsmart@gmail.com>
2026-02-03 12:06:29 +0530

    object h1 in js to html , painting css

--- E1/app.js
+++ E1/app.js
@@ -1,7 +1,10 @@
 const heading = React.createElement(
     "h1", 
     {id: "heading", xyz: "abc"}, 
-    "Hello World");
+    "Hello World"); // this not a heading yet it is an object
+
+    console.log(heading);
+    
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
